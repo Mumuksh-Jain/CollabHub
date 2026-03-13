8fe2253546639b876ecc1a6518dc8e9a03f74105
Moksh <mumukshujain2466@gmail.com>
2026-03-13 11:32:40 +0530

    fix cors for vercel frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,10 +6,28 @@ const authRoutes=require("../src/routes/auth.route")
 const projectRoutes=require("../src/routes/project.routes")
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({
-  origin: ['http://localhost:5173', "https://collab-hub-beige.vercel.app/"],
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: function (origin, callback) {
+
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://collab-hub1.vercel.app"
+      ];
+
+      if (
+        !origin || 
+        allowedOrigins.includes(origin) ||
+        origin.endsWith(".vercel.app")
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  })
+);
 app.use("/api/auth",authRoutes)
 app.use("/api/project",projectRoutes)
 
