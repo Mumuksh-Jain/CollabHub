46ebe5f7270e473829193ae0375dfa31380504f9
Moksh <mumukshujain2466@gmail.com>
2026-03-13 11:58:51 +0530

    fix frontend  issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // important for correct paths
+  base: "/",      // important for correct relative paths
+  build: {
+    outDir: "dist" // default is dist, just make sure
+  }
 });
