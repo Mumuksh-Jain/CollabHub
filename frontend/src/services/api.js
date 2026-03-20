05f8c2e22fc29c35f72477966984ede3c3f90cfb
Moksh <mumukshujain2466@gmail.com>
2026-03-20 19:45:25 +0530

    Some Changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://collabhub-backend-xros.onrender.com/api",
   withCredentials: true,
 });
 
