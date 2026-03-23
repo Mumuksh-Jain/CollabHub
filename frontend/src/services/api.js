92e2d876f6533b7354fc0d08917420f47c798f01
Moksh <mumukshujain2466@gmail.com>
2026-03-23 20:00:30 +0530

    Fixed api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:import.meta.env.VITE_BACKEND_URL || "https://collabhub-backend-xros.onrender.com/api"
+  baseURL:import.meta.env.VITE_BACKEND_URL || "https://collabhub-backend-xros.onrender.com/api",
   withCredentials: true,
 });
 
